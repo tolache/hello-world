d5ad0bdafd8c8463910b3061eceb95d709346293
tolache <anatoly.cherenkov@gmail.com>
2019-01-18 14:08:51 +0300

    fix one too few lines bug

--- Hello_World/Hello_World/HelloSayer.cs
+++ Hello_World/Hello_World/HelloSayer.cs
@@ -6,8 +6,8 @@ namespace Hello_World
     {
         public static void SayHello(string name, int linesCount)
         {
-            for (int i = 1; i < linesCount; i++)
-            Console.WriteLine("Hello {0}! This is line #{1}.", name, i);
+            for (int i = 1; i <= linesCount; i++)
+                Console.WriteLine("Hello {0}! This is line #{1}.", name, linesCount);
         }
     }
 }
