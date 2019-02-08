29ba574b47cdcf3c4d9cd0afee54df38890cc7d7
tolache <anatoly.cherenkov@gmail.com>
2019-02-08 15:40:26 +0300

    Change lines to 1

--- Hello_World/Hello_World/Program.cs
+++ Hello_World/Hello_World/Program.cs
@@ -6,7 +6,7 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            HelloSayer.HelloSayer.SayHello("World", 10);
+            HelloSayer.HelloSayer.SayHello("World", 1);
             Console.ReadKey();
         }
     }
