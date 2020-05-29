cda8f741bfc9630dd79f01852f57ce6cba9a1af3
tolache <anatoly.cherenkov@gmail.com>
2020-05-29 16:49:21 +0300

    add files

---
+++ Hello_World/Account.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Hello_World
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
